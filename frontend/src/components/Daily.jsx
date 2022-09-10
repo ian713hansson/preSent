ff4523bf9e2dd9291edf899777657812c7ad4425
Ian Hansson <ian713hansson@gmail.com>
2022-09-10 14:38:29 -0500

    axios call for daily words on home page

---
+++ frontend/src/components/Daily.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+const DailyWords = (props) => {
+    console.log(props)
+
+    return (
+        <div className="daily_words" >
+            <h2>{props.name}</h2>
+            <p className="daily_body">{props.body}</p>
+        </div>
+    )
+}
+
+export default DailyWords
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,40 @@
+import React from "react"
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import DailyWords from "../components/Daily"
 
 
+    
 
 const Home = () => {
+
+    const [daily_words, setDailyWords] = useState([])
+
+    useEffect(()=>{
+        const getDailyWords = async () => {
+          let res = await axios.get('http://localhost:8000/api/daily_words/')
+          console.log(res.data)
+          setDailyWords(res.data)
+        }
+        getDailyWords()
+      }, []) 
+
     return (
         <div className="home">
             <h1>Enjoy a space to look inward and outward</h1>
             <p>Inside you will find an ever expanding universe full of tools to help you manifest peaceful awareness.</p>
             <br></br>
-            <p>The notion of ruling your world is that you can live in a dignified and disciplined way, without frivolity, and at the same time enjoy your life. You can combine survival and celebration.
-                -Chogyam Trungpa</p>
+            <section className="daily_words_grid">
+                {daily_words.map((daily_words)=>(
+                <div>
+                    <DailyWords 
+                        key={daily_words.name}
+                        name={daily_words.name}
+                        body={daily_words.body}
+                    />
+                </div>
+                ))}
+            </section>
         </div>
     )
 }
