2116ad7b2e11ce2bf53bb32bfe012208ed80e59e
Ian Hansson <ian713hansson@gmail.com>
2022-09-13 09:40:34 -0500

    styling logo, header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 const Header = () => {
 
   return (
-    <header>
+    <header className='header_container'>
         <div className='header'>
-            <h1>preSent</h1>
-            <h4>A Cyber Sangha for inhabitants of the fringe</h4>
+            <img className='logo' src='https://i.imgur.com/lhjDisq.png' />
+            
             <div className='header_img_container'>
               <img className='header_img' src='https://i.imgur.com/25pHtQy.png' />
             </div>
-            
+            </div>
         <div className='nav_header'>
             <nav>
                 <Link className='nav_links' to="/">Home</Link>
@@ -18,8 +18,9 @@ const Header = () => {
                 <Link className='nav_links' to="/creators">About Us</Link>
                 <Link className='nav_links' to="/newpost">New Post</Link>
             </nav>
-        </div>    
-      </div>
+        </div>  
+          
+      
     </header>
   )
 }
