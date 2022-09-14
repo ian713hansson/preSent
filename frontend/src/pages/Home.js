ed674df05848802937a20aac9589845bdf4e8fae
Ian Hansson <ian713hansson@gmail.com>
2022-09-14 08:48:50 -0500

    update readme

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
         
     return (
         <div className="home">
-            <h1>Enjoy a space to look inward and outward</h1>
+            <h1>A Cyber Sangha</h1>
             <p>Inside you will find an ever expanding universe full of tools to help you manifest peaceful awareness.</p>
             <br></br>
             <section className="daily_words_grid">
